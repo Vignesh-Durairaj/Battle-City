050c6cb43ed210d6ce6b0e030750ea8a9833dac5
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-20 02:07:24 +0530

    Test URL inclusion

--- src/main/java/clientlib/ClientApp.java
+++ src/main/java/clientlib/ClientApp.java
@@ -4,7 +4,8 @@ import com.dojo.tank.solver.BasicSolver;
 
 public class ClientApp {
 
-    private final static String URL = "http://dojorena.io/codenjoy-contest/board/player/zwef9wxfk60zs3zxbkia?code=7523947537256409738&gameName=battlecity";
+    final static String URL = "http://dojorena.io/codenjoy-contest/board/player/zwef9wxfk60zs3zxbkia?code=7523947537256409738&gameName=battlecity";
+    final static String TEST_URL = "http://10.22.19.165:8080/codenjoy-contest/board/player/rbye7jraqg9ouwyeg6hp?code=8499226565780091758&gameName=battlecity";
 
     public static void main(String[] args) {
         try {
